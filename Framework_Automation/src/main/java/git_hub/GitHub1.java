d708c568e1e63a674250c81cc889a6b7057806ed
Admin <Admin@DESKTOP-DEE2I8N>
2024-11-15 19:25:42 +0530

    Welcome Selenium

---
+++ Framework_Automation/src/main/java/git_hub/GitHub1.java
@@ -0,0 +1,11 @@
+package git_hub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHub1 {
+       @Test
+       public void Demo() {
+    	   Reporter.log("Welcome selenium", true);
+       }
+}
